47a7868bc48bc28ccbf8700dec0770fffc4ab207
Anzhelika Volosova <agvolosova@miem.hse.ru>
2022-10-15 18:15:13 +0400

    git commit hi

--- mintests2.py
+++ mintests2.py
@@ -5,6 +5,7 @@ import time
 import math
 
 file = [i for i in range(10000)]
+file1 = [i for i in range(100000)]
 
 
 class MinTest(TestCase):
@@ -27,6 +28,20 @@ class MinTest(TestCase):
         time_start = time.time()
         self.assertEqual(Program._mult(self, file), 0)
         print("Время работы функции нахождения произведения:", time.time() - time_start, "секунд")
+
+    def test_log(self):
+        self.assertTrue(Program._max(self, file) >= Program._min(self, file))
+
+    def test_time(self):
+        time_start = time.time()
+        Program._max(self, file)
+        time_file = time.time()
+        Program._max(self, file1)
+        time_file1 = time.time()
+        print("Время работы функции нахождения минимума:", time_file - time_start, "секунд")
+        print("Время работы функции нахождения минимума для количества чисел в 10 раз больше:", time_file1 - time_file, "секунд")
+        self.assertTrue(time_file - time_start <= time_file1 - time_file)
+
 
 if __name__ == '__main__':
     main()
