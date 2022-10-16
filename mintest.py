41af93ff507e6c67793e1330006430cc30f2f00e
Anzhelika Volosova <agvolosova@miem.hse.ru>
2022-10-16 12:10:46 +0400

    добавление тестов по одному

---
+++ mintest.py
@@ -0,0 +1,16 @@
+from unittest import TestCase, main
+from tz2 import Program
+
+
+file = [i for i in range(10000)]
+
+
+class MinTest(TestCase):
+    def test_min(self):
+        time_start = time.time()
+        self.assertEqual(Program._min(self, file), min(file))
+        print("Время работы функции нахождения минимума:", time.time() - time_start, "секунд")
+
+
+if __name__ == '__main__':
+    main()
